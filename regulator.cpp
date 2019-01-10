467f6fe5c537301100e8d480b028b1faff3f6f56
friko9 <friko9@gmail.com>
2019-01-10 13:27:17 +0100

    regulator.cpp: Removed IO from regulator loop; debugged ins/out logic; time-based logging of buffSize/inFlow/outFlow in main process; implemented process joining; insThread/remThread now hooked up with cin/cout streams

--- regulator.cpp
+++ regulator.cpp
@@ -13,43 +13,51 @@ template<typename T>
 class threadsafe_queue
 {
 private:
-    mutable std::mutex mut;
-    std::queue<T> data_queue;
+  mutable std::mutex mut;
+  std::queue<T> data_queue;
+  size_t size_limit = 1000000;
 public:
-    threadsafe_queue()
-    {}
+  threadsafe_queue()
+  {}
 
-    void push(T new_value)
-    {
-        std::lock_guard<std::mutex> lk(mut);
-        data_queue.push(std::move(new_value));
-    }
-
+  bool push(T new_value)
+  {
+    std::lock_guard<std::mutex> lk(mut);
+    if ( full() )
+      return false;
+    data_queue.push(std::move(new_value));
+    return true;
+  }
   bool wait_and_pop(T& value)
-    {
-      std::lock_guard<std::mutex> lk(mut);
-	if( empty() )
-	  return false;
-        value=std::move(data_queue.front());
-        data_queue.pop();
-	return true;
-    }
-
-    bool empty() const
-    {
-      return data_queue.empty();
-    }
+  {
+    std::lock_guard<std::mutex> lk(mut);
+    if( empty() )
+      return false;
+    value=std::move(data_queue.front());
+    data_queue.pop();
+    return true;
+  }
   size_t size()
   {
     std::lock_guard<std::mutex> lk(mut);
     return data_queue.size();
+  }
+  void set_size_limit(size_t limit){ size_limit = limit; }
+private:
+  bool full() const
+  {
+    return data_queue.size() >= size_limit;
+  }
+  bool empty() const
+  {
+    return data_queue.empty();
   }
 };
 
 template <typename RegT,typename CtrlT,typename RegSPT,typename CtrlSPT>
 class Regulator
 {
-  RegT& m_reg;
+  const RegT& m_reg;
   CtrlT& m_ctrl;
   float m_k;
   RegSPT m_reg0;
@@ -57,15 +65,17 @@ class Regulator
 public:
   Regulator(RegT& reg,CtrlT& ctrl,RegSPT reg0, CtrlSPT ctrl0,float k)
     : m_reg(reg),m_reg0(reg0),m_ctrl(ctrl),m_ctrl0(ctrl0),m_k(k)
-  {}
-  //  Regulator(const Regulator&) = default;
-  //Regulator(Regulator&&) = default;
+  {
+    m_ctrl = 0;
+  }
+  Regulator(const Regulator&) = default;
+  Regulator(Regulator&&) = default;
   ~Regulator() = default;
 public:
   void operator()(){ update(); }
   void update()
   {
-    m_ctrl = m_ctrl0 - (m_k*(m_reg - m_reg0));
+    m_ctrl = m_ctrl0 - static_cast<CtrlT>(m_k*(m_reg - m_reg0));
   }
   void setpoint(RegSPT reg0, CtrlSPT ctrl0)
   {
@@ -82,8 +92,13 @@ Regulator<RegT,CtrlT,RegSPT,CtrlSPT> makeRegulator(RegT& reg,CtrlT& ctrl,RegSPT 
 
 std::atomic<int> diffFlow;
 std::atomic<int> buffSize;
+std::atomic<bool> finFlag;
 threadsafe_queue<int> tsq;
-constexpr int setpointFlow = 100;
+
+std::atomic<int> inFlow;
+std::atomic<int> outFlow;
+
+constexpr int setpointFlow = 200;
 constexpr int setpointSize1 = 10000;
 constexpr int setpointSize2 = 50000;
 
@@ -91,58 +106,88 @@ using namespace std;
 
 int main(int argc,const char* argv[])
 {
-  float k = (argc == 2)? std::stof(argv[1]) : 1;
+  float k = (argc == 2)? std::stof(argv[1]) : 0.0f;
   auto reg = makeRegulator(buffSize,diffFlow,setpointSize1,setpointFlow,k);
+  finFlag = false;
   std::thread regThread([&reg](){
-			  int x =0;
-			  while(true)
+			  while(!finFlag)
 			    {
 			      buffSize = tsq.size();
 			      reg.update();
-			      cerr<<x++<<' '<<diffFlow<<' '<<tsq.size()<<endl;
-			      if(x >= 10000) exit(1);
 			      std::this_thread::sleep_for(std::chrono::milliseconds(1));
 			    }
 			});
   std::thread insThread([](){
 			  int x=0;
-			  int local_diffFlow;
-			  while(true)
+			  x = cin.get();
+			  while(!finFlag)
 			    {
-			      local_diffFlow = diffFlow.load();
-			      for(int i=0; i< setpointFlow + local_diffFlow/2; ++i)
-				tsq.push(++x);
-			      std::this_thread::yield();
-			      //			      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			      int success = 0;
+			      int flow = setpointFlow + diffFlow.load()/2;
+			      flow = (flow >=0 )? flow : 0;
+			      for(int i=0; i< flow; ++i)
+				{
+				  if( !tsq.push(x) ) break;
+				  x = cin.get();
+				  ++success;
+				}
+			      inFlow += success;
+			      //std::this_thread::yield();
+			      //std::this_thread::sleep_for(std::chrono::milliseconds(10));
 			    }
 			});
-    std::thread remThread([](){
+  std::thread remThread([](){
 			  int x=0;
-			  int local_diffFlow;
-			  while(true)
+			  while(!finFlag)
 			    {
-			      local_diffFlow = diffFlow.load();
-			      for(int i=0; i< setpointFlow - local_diffFlow/2; ++i)
+			      int success = 0;
+			      int flow = setpointFlow - diffFlow.load()/2;
+			      flow = (flow >=0 )? flow : 0;
+			      for(int i=0; i< flow; ++i)
 				{
-				  if(tsq.wait_and_pop(x))
-				    cout<<x<<endl;
+				  if( !tsq.wait_and_pop(x) ) break;
+				  cout<<x;
+				  ++success;
 				}
-			      std::this_thread::yield();
+			      outFlow += success;
+			      //std::this_thread::yield();
 			      //std::this_thread::sleep_for(std::chrono::milliseconds(20));
 			    }
 			});
-    std::thread chBufSetpoint([&reg]()
-			      {
-				int x = false;
-				while(true)
+  std::thread chBufSetpoint([&reg,k]()
+  			    {
+  			      int x = false;
+				while(!finFlag)
 				  {
 				    if( x = !x )
-				      reg.setpoint(setpointSize1,setpointFlow);
+				      {
+					tsq.set_size_limit(setpointSize1*1.2);
+					reg.setpoint(setpointSize1,setpointFlow);
+				      }
 				    else
-				      reg.setpoint(setpointSize2,setpointFlow);
+				      {
+					tsq.set_size_limit(setpointSize2*1.2);
+					reg.setpoint(setpointSize2,setpointFlow);
+				      }
 				    std::this_thread::sleep_for(std::chrono::seconds(1));
 				  }
-			      });
+  			    });
+
+  int x = 0;
+  auto start = std::chrono::high_resolution_clock::now();
+  cerr<<"time buffSize inFlow outFlow\n";
+  long counter = 0;
+  while( ++x < 10000)
+    {
+      auto now = std::chrono::high_resolution_clock::now();
+      auto time = std::chrono::duration_cast<std::chrono::milliseconds>(now-start);
+      cerr<<time.count()<<' '<<buffSize<<' '<<inFlow<<' '<<outFlow<<'\n';
+      counter+= outFlow;
+      inFlow = 0;
+      outFlow = 0;
+      std::this_thread::sleep_until(now + std::chrono::milliseconds(1));
+    }
+  finFlag = true;
   regThread.join();
   insThread.join();
   remThread.join();
